f9570897150bcb0ee8a4c2bcd04b101aedc12abb
Mingshan Lei <leimingshan116@gmail.com>
2016-02-09 00:05:18 +0800

    update: test case

--- src/test/java/com/iscas/pminer/service/ProfileLevelHelperTest.java
+++ src/test/java/com/iscas/pminer/service/ProfileLevelHelperTest.java
@@ -1,8 +1,11 @@
 package com.iscas.pminer.service;
 
+import com.iscas.pminer.entity.OfficeRecord;
 import com.iscas.pminer.entity.Profile;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
@@ -43,6 +46,13 @@ public class ProfileLevelHelperTest {
         when(testProfile2.getGender()).thenReturn("");
         assertEquals(helper.getLevel(testProfile2), 3);
 
+        when(testProfile2.getGender()).thenReturn("男");
+        when(testProfile2.getNation()).thenReturn("汉");
+        assertEquals(helper.getLevel(testProfile2), 3);
+        when(testProfile2.getAge()).thenReturn("30");
+        when(testProfile2.getNation()).thenReturn("-");
+        assertEquals(helper.getLevel(testProfile2), 3);
+
         Profile testProfile3 = mock(Profile.class);
         when(testProfile3.getName()).thenReturn("张三");
         when(testProfile3.getGender()).thenReturn("男");
@@ -53,6 +63,35 @@ public class ProfileLevelHelperTest {
 
         when(testProfile3.getBirthProvince()).thenReturn(null);
         assertEquals(helper.getLevel(testProfile3), 3);
+
+        Profile testProfile4 = mock(Profile.class);
+        when(testProfile4.getName()).thenReturn("李四");
+        when(testProfile4.getGender()).thenReturn("男");
+        when(testProfile4.getNation()).thenReturn("汉");
+        when(testProfile4.getAge()).thenReturn("30");
+        when(testProfile4.getBirthProvince()).thenReturn("河南省");
+        when(testProfile4.getBirthDate()).thenReturn("---");
+        when(testProfile4.getWorkDate()).thenReturn("19900101");
+        assertEquals(helper.getLevel(testProfile4), 2);
+        when(testProfile4.getBirthDate()).thenReturn("19500101");
+        when(testProfile4.getWorkDate()).thenReturn("---");
+        assertEquals(helper.getLevel(testProfile4), 2);
+
+        when(testProfile4.getBirthDate()).thenReturn("19500101");
+        when(testProfile4.getWorkDate()).thenReturn("---");
+        assertEquals(helper.getLevel(testProfile4), 2);
+
+        when(testProfile4.getBirthDate()).thenReturn("19500101");
+        when(testProfile4.getWorkDate()).thenReturn("19790101");
+        when(testProfile4.getOfficeRecord()).thenReturn(null);
+        assertEquals(helper.getLevel(testProfile4), 2);
+        List<OfficeRecord> list = mock(List.class);
+        when(list.size()).thenReturn(3);
+        when(testProfile4.getOfficeRecord()).thenReturn(list);
+        assertEquals(helper.getLevel(testProfile4), 2);
+        when(list.size()).thenReturn(8);
+        System.out.println(testProfile4.getOfficeRecord().size());
+        assertEquals(helper.getLevel(testProfile4), 1);
     }
 
 }
